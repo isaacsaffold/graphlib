47dcff0657ffee5593aa25a27daf1364ca193105
Isaac <ijsaffold@mail.lipscomb.edu>
2019-09-29 02:51:00 -0500

    changed underlying type to unsigned char

--- constraints.hpp
+++ constraints.hpp
@@ -4,35 +4,39 @@
 namespace graph
 {
     // conforms to `BitmaskType`
-    enum class Constraints: unsigned int
+    enum class Constraints: unsigned char
     {
         NONE = 0,
         NO_LOOPS = 1,
         NO_MULTIPLE_EDGES = 2,
-        SIMPLE = 3
+        SIMPLE = 3,
+        CONNECTED = 4,
+        CONNECTED_AND_NO_LOOPS = 5,
+        CONNECTED_AND_NO_MULTIPLE_EDGES = 6,
+        SIMPLY_CONNECTED = 7
     };
 
     constexpr Constraints operator&(Constraints a, Constraints b)
     {
-        return static_cast<Constraints>(static_cast<unsigned int>(a) & static_cast<unsigned int>(b));
+        return static_cast<Constraints>(static_cast<unsigned char>(a) & static_cast<unsigned char>(b));
     }
 
     constexpr Constraints operator|(Constraints a, Constraints b)
     {
-        return static_cast<Constraints>(static_cast<unsigned int>(a) | static_cast<unsigned int>(b));
+        return static_cast<Constraints>(static_cast<unsigned char>(a) | static_cast<unsigned char>(b));
     }
 
     constexpr Constraints operator^(Constraints a, Constraints b)
     {
-        return static_cast<Constraints>(static_cast<unsigned int>(a) ^ static_cast<unsigned int>(b));
+        return static_cast<Constraints>(static_cast<unsigned char>(a) ^ static_cast<unsigned char>(b));
     }
 
-    constexpr Constraints operator~(Constraints a) {return static_cast<Constraints>(~static_cast<unsigned int>(a));}
+    constexpr Constraints operator~(Constraints a) {return static_cast<Constraints>(~static_cast<unsigned char>(a));}
 
-    constexpr bool operator==(Constraints a, unsigned int b) {return static_cast<unsigned int>(a) == b;}
-    constexpr bool operator==(unsigned int a, Constraints b) {return a == static_cast<unsigned int>(b);}
-    constexpr bool operator!=(Constraints a, unsigned int b) {return !(a == b);}
-    constexpr bool operator!=(unsigned int a, Constraints b) {return !(a == b);}
+    constexpr bool operator==(Constraints a, unsigned char b) {return static_cast<unsigned char>(a) == b;}
+    constexpr bool operator==(unsigned char a, Constraints b) {return a == static_cast<unsigned char>(b);}
+    constexpr bool operator!=(Constraints a, unsigned char b) {return !(a == b);}
+    constexpr bool operator!=(unsigned char a, Constraints b) {return !(a == b);}
 
     Constraints& operator&=(Constraints& a, Constraints b) {return a = a & b;}
     Constraints& operator|=(Constraints& a, Constraints b) {return a = a | b;}
