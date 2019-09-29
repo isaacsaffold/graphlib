3f03ec82e6eaee6449856509c49c6edb6134c4fe
Isaac <ijsaffold@mail.lipscomb.edu>
2019-09-29 04:18:53 -0500

    added 'ConstraintViolationException'

--- constraints.hpp
+++ constraints.hpp
@@ -1,6 +1,8 @@
 #ifndef GRAPH_CONSTRAINTS_HPP_9292019
 #define GRAPH_CONSTRAINTS_HPP_9292019
 
+#include <stdexcept>
+
 namespace graph
 {
     // conforms to `BitmaskType`
@@ -41,6 +43,41 @@ namespace graph
     Constraints& operator&=(Constraints& a, Constraints b) {return a = a & b;}
     Constraints& operator|=(Constraints& a, Constraints b) {return a = a | b;}
     Constraints& operator^=(Constraints& a, Constraints b) {return a = a ^ b;}
+
+    class ConstraintViolationException: public std::runtime_error
+    {
+        private:
+            static const char* exceptionString(Constraints constraints)
+            {
+                switch (constraints)
+                {
+                    case Constraints::NO_LOOPS:
+                        return "Graph cannot contain loops.";
+                    case Constraints::NO_MULTIPLE_EDGES:
+                        return "Graph cannot contain multiple edges.";
+                    case Constraints::SIMPLE:
+                        return "Graph must be simple (i.e. must not contain loops or multiple edges).";
+                    case Constraints::CONNECTED:
+                        return "Graph must be connected.";
+                    case Constraints::CONNECTED_AND_NO_LOOPS:
+                        return "Graph must be connected and cannot contain loops.";
+                    case Constraints::CONNECTED_AND_NO_MULTIPLE_EDGES:
+                        return "Graph must be connected and cannot contain multiple edges.";
+                    case Constraints::SIMPLY_CONNECTED:
+                        return "Graph must be connected and simple (i.e. must not contain loops or multiple edges).";
+                    default:
+                        return "";
+                }
+            }
+
+        public:
+            using std::runtime_error::runtime_error;
+
+            explicit ConstraintViolationException(Constraints constraints) noexcept:
+                std::runtime_error(exceptionString(constraints)) {}
+
+            virtual ~ConstraintViolationException() = default;
+    };
 }
 
 #endif
